c126940977badae42141bcd2d7efccf9dc5fcfb6
LeraNickolaeva <leranick29@gmail.com>
2016-03-07 23:23:30 +0300

    add registration

---
+++ src/controller/command/implementation/author/OpenAuthorCommand.java
@@ -0,0 +1,34 @@
+package controller.command.implementation.author;
+
+import controller.command.Command;
+import dao.AuthorDao;
+import dao.implementation.AuthorDaoImpl;
+import domain.Author;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by Vika on 3/1/2016.
+ */
+public class OpenAuthorCommand implements Command {
+
+    private static final Command instance = new OpenAuthorCommand();
+
+    private OpenAuthorCommand(){}
+
+    public static Command getInstance() {
+        return instance;
+    }
+
+    @Override
+    public String execute(HttpServletRequest request) {
+        AuthorDao authorDao = AuthorDaoImpl.getInstance();
+        int id = Integer.parseInt(request.getParameter("author_id"));
+        Author author = authorDao.read(id);
+
+        request.setAttribute("author", author);
+
+        return "author.jsp";
+
+    }
+}
